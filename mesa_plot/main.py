50571767d73e14d78082d3cc7e168d3116539cc8
Charlie-Giese <cmggiese@gmail.com>
2022-01-06 17:16:46 +0100

    fix

--- mesa_plot/main.py
+++ mesa_plot/main.py
@@ -73,7 +73,7 @@ class history_plot():
         fig, axs = plt.subplots(2,1)
 
         for ax in axs:
-            ax.set_xscale('log')
+            #ax.set_xscale('log')
             ax.set_yscale('log')
 
         axs[0].plot(age, LH, label = 'H Luminosity', c='k')
