217f71fc1bab6d314cff555d44451583979c26fa
Charlie-Giese <cmggiese@gmail.com>
2022-01-06 15:01:45 +0100

    fix

--- mesa_plot/main.py
+++ mesa_plot/main.py
@@ -228,14 +228,14 @@ class ZAMS_TAMS():
                 indices[1] = p
                 break
         """
-        indices  = np.argwhere(power_nuc > 1e-3)
+        indices  = np.argwhere(power_nuc > 1e-8)
 
         fig = plt.figure()
         ax = fig.add_subplot(211)
         ax.set_xscale('log')
         ax.set_yscale('log')
         ax.scatter(star_age[indices], h_power[indices], c='k')
-        ax.plot(star_age[indices], power_nuc[indices], '--r', c='r')
+        #ax.plot(star_age[indices], power_nuc[indices], '--r', c='r')
         ax.set_ylabel('Power')
         ax.set_xlabel('Stellar Age (Years)')
 
